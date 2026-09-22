a3e70c00bb3f64092044ae08242d5604d3588e96
Juan Marín Escolano <juanmarinescolano@gmail.com>
2026-09-22 18:11:28 +0200

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/queueflow/QueueflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.queueflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueflowBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/queueflow/QueueflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queueflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
